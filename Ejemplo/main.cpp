ef4d5d371373b72b1fdb2c712de9c427011018ae
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-11-06 12:15:06 -0600

    Multiplica dos numeros, usando una funcion recursiva.

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -4,26 +4,28 @@ using namespace std;
 int multiplica(int a, int b);
 
 int main(void){
-    int a=7,b=8,c=0;
+    int a=7,b=8,c;
 
-    cout<<endl<<endl
-        <<"main CALCULA ITERATIVAMENTE "<<a<<"*"<<b
-        <<endl<<endl;
+    cout<<"main INVOCA UNA VEZ A UNA FUNCION RECURSIVA PARA CALCULAR "<<a<<"*"<<b<<endl<<endl;
 
-    while(b>=1){
-        c = c + multiplica(a,b);
-        cout<<"main calcula c = "<<c<<endl;
-        b--;
-    }
+    c = multiplica(a,b);
 
-    cout<<endl<<endl<<"RESULTADO: "<<c<<endl;
+    cout<<endl<<endl<<"main RECIBE DE LA UNICA FUNCION RECURSIVA QUE INVOCA: "<<c<<endl;
 
     return 0;
 }
 
 int multiplica(int a, int b){
 
-    cout<<"Misma funcion, recibe "<<a<<" y "<<b<<". Retorna "<<a<<". ";
+    if(b>1){
+        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
+            <<". Retorna "<<a<<" + lo que regrese otra con "<<a<<" y "<<b-1<<endl;
+        return a + multiplica(a, b-1);
+        }
+    else{
+        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
+            <<". Retorna "<<a;
+        return a;
+    }
 
-    return a;
 }
