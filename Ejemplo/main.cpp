06f54100c85ba04f87f3dfae4640e22026a8c3cb
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-11-06 12:32:18 -0600

    Calcula a^b, usando una funcion recursiva

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -4,26 +4,28 @@ using namespace std;
 int potencia(int a, int b);
 
 int main(void){
-    int a=2,b=8,c=1;
+    int a=2,b=8,c;
 
-    cout<<endl<<endl
-        <<"main CALCULA ITERATIVAMENTE "<<a<<"^"<<b
-        <<endl<<endl;
+    cout<<"main INVOCA UNA VEZ A UNA FUNCION RECURSIVA PARA CALCULAR "<<a<<"^"<<b<<endl<<endl;
 
-    while(b>=1){
-        c = c * potencia(a,b);
-        cout<<"main calcula c = "<<c<<endl;
-        b--;
-    }
+    c = potencia(a,b);
 
-    cout<<endl<<endl<<"RESULTADO: "<<c<<endl;
+    cout<<endl<<endl<<"main RECIBE DE LA UNICA FUNCION RECURSIVA QUE INVOCA: "<<c<<endl;
 
     return 0;
 }
 
 int potencia(int a, int b){
 
-    cout<<"Misma funcion, recibe "<<a<<" y "<<b<<". Retorna "<<a<<". ";
+    if(b>0){
+        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
+            <<". Retorna "<<a<<" * lo que regrese otra con "<<a<<" y "<<b-1<<endl;
+        return a * potencia(a, b-1);
+        }
+    else{
+        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
+            <<". Retorna "<<1;
+        return 1;
+    }
 
-    return a;
 }
