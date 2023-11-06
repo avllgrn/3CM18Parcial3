2e8cee50ac5a52133ced6075482ba40f848c044e
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-11-06 11:59:35 -0600

    Multiplica dos numeros, usando iterativamente una funcion.

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int multiplica(int a, int b);
+
 int main(void){
-	
-	cout<<"Hola, mundo!"<<endl;
-	
+    int a=7,b=8,c=0;
+
+    cout<<endl<<endl
+        <<"main CALCULA ITERATIVAMENTE "<<a<<"*"<<b
+        <<endl<<endl;
+
+    while(b>=1){
+        c = c + multiplica(a,b);
+        cout<<"main calcula c = "<<c<<endl;
+        b--;
+    }
+
+    cout<<endl<<endl<<"RESULTADO: "<<c<<endl;
+
     return 0;
+}
+
+int multiplica(int a, int b){
+
+    cout<<"Misma funcion, recibe "<<a<<" y "<<b<<". Retorna "<<a<<". ";
+
+    return a;
 }
