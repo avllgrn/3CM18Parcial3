65e1d2a0cc28ab443c232f6721cc10d828302420
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-11-08 14:53:32 -0600

    factorial(n) iterativamente

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int potencia(int a, int b);
+long long factorial(int n);
 
 int main(void){
-    int a=2,b=8,c;
+    int x=7;
 
-    cout<<"main INVOCA UNA VEZ A UNA FUNCION RECURSIVA PARA CALCULAR "<<a<<"^"<<b<<endl<<endl;
-
-    c = potencia(a,b);
-
-    cout<<endl<<endl<<"main RECIBE DE LA UNICA FUNCION RECURSIVA QUE INVOCA: "<<c<<endl;
+    cout<<x<<"! = f("<<x<<") = "<<factorial(x)<<endl;
 
     return 0;
 }
 
-int potencia(int a, int b){
+long long factorial(int n){
+    long long f=1;
 
-    if(b>0){
-        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
-            <<". Retorna "<<a<<" * lo que regrese otra con "<<a<<" y "<<b-1<<endl;
-        return a * potencia(a, b-1);
-        }
-    else{
-        cout<<"Una funcion recursiva, recibe "<<a<<" y "<<b
-            <<". Retorna "<<1;
-        return 1;
-    }
+    for(int i=2; i<=n; i++)
+        f *= i;
 
+    return f;
 }
