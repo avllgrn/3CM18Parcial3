03102d2a486244b919e29979851dc18e2e0f51d6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-11-08 14:55:36 -0600

    fatorial(n) recursiva

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
-long long factorial(int n);
+long long factorial(long long n);
 
 int main(void){
     int x=7;
-
-    cout<<x<<"! = f("<<x<<") = "<<factorial(x)<<endl;
-
+    cout<<endl<<x<<"! = f("<<x<<") = "<<factorial(x)<<endl;
     return 0;
 }
-
-long long factorial(int n){
-    long long f=1;
-
-    for(int i=2; i<=n; i++)
-        f *= i;
-
-    return f;
+long long factorial(long long n){
+    long long f;
+    cout<<"f("<<n<<")"<<endl;
+    if(n>0){
+        f = n * factorial(n-1);
+        cout<<"f("<<n<<") retorna "<<f<<endl;
+        return f;
+    }
+    else{
+        cout<<endl<<"f("<<n<<") retorna "<<1<<endl;
+        return 1;
+    }
 }
