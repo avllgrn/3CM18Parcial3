fcd88936446a482b44718500e755be52bfd78124
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-12-15 14:31:53 -0600

    Escribir y leer distintos datos hacia y desde un archivo.

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -4,18 +4,28 @@ using namespace std;
 
 int main(void){
     ofstream FSalida("archivo.txt");
-    string a = "UnaCadenaCualquiera.";
+    int a = 5;
+    float b = 56.78;
+    string c = "UnaCadenaCualquiera.";
 
     FSalida << a << endl;
+    FSalida << b << endl;
+    FSalida << c << endl;
 
     FSalida.close();
 
 
     ifstream FEntrada("archivo.txt");
+    int otroInt;
+    float otroFloat;
     string otroString;
 
-    FEntrada >> otroString;
+    FEntrada >> otroInt
+             >> otroFloat
+             >> otroString;
 
+    cout << otroInt << endl;
+    cout << otroFloat << endl;
     cout << otroString << endl;
 
     FEntrada.close();
