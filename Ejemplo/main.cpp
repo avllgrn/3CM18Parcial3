33086d927fd219fdb4bf2e8f5a7b4d7a4f6155b9
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-12-18 15:32:35 -0600

    Extraer varias subcadenas numericas de una cadena

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
 #include <sstream>
 using namespace std;
 
 int main(void){
-    string Cadena;
-    int entero;
-    char caracter;
+    string VariosEnterosEnCadena;
+    string UnEnteroEnCadena;
+    char separador=',';
+
     cout<<"Ingresa varios enteros cada uno seguido por una coma ";
-    getline(cin,Cadena);
+    getline(cin,VariosEnterosEnCadena);//Se guardan todos los numeros como una sola cadena de caracteres numericos separados por coma
 
-    cout<<"Ingresaste: "<<Cadena<<endl<<endl;
-
-    stringstream ss(Cadena);
+    stringstream ss(VariosEnterosEnCadena); //Se conecta un canal (stringstream)
+                                            //hacia la cadena de caracteres numericos separados por coma
 
     cout<<"Cada numero por separado:"<<endl;
-    while(ss>>entero>>caracter){
-        cout<<entero<<endl;
+    while( !ss.eof() ){ //Mientras el canal no haya llegado al fin del flujo de caracteres numericos,
+
+        getline(ss, UnEnteroEnCadena, separador);   //se extrae del canal una cadena numerica
+                                                    //desde una posicion hasta encontrarse el separador(la coma).
+
+        cout<<UnEnteroEnCadena<<endl;//Se muestra cada cadena de caracteres numericos extraida
     }
 
     return 0;
